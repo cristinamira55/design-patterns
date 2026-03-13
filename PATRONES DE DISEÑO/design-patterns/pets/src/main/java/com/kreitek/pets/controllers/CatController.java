19dc6ba9e24c54cf9f5236a276db62ecd2eafc79
Cristina Mira Montesino <cristina.mira@nunsys.com>
2026-03-13 11:31:24 +0100

    añado ejercicios y organizado por carpetas

---
+++ PATRONES DE DISEÑO/design-patterns/pets/src/main/java/com/kreitek/pets/controllers/CatController.java
@@ -0,0 +1,30 @@
+package com.kreitek.pets.controllers;
+import com.kreitek.pets.Controller;
+import com.kreitek.pets.domain.Cat;
+import com.kreitek.pets.infraestructure.bd.DbService;
+import com.kreitek.pets.Logger;
+import java.util.List;
+
+public class CatController implements Controller {
+
+    @Override
+    public String executePut(String petName, String ownerName, String telephone) {
+        Logger.debug("CatController.executePut " + petName + "," + ownerName + "," + telephone);
+        Cat cat = new Cat(petName, ownerName, telephone);
+        DbService dbService = DbService.getInstance();
+        dbService.addNewCat(cat);
+        return "New cat has been added";
+    }
+
+    @Override
+    public String executeGet() {
+        Logger.debug("CatController.executeGet CATS");
+        DbService dbService = DbService.getInstance();
+        List<Cat> cats = dbService.getCats();
+        String response = "";
+        for (Cat cat:cats) {
+            response += cat.toString() + "\r\n";
+        }
+        return response;
+    }
+}
---
+++ PATRONES DE DISEÑO/design-patterns/pets/src/main/java/com/kreitek/pets/infraestructure/bd/DbService.java
@@ -0,0 +1,50 @@
+package com.kreitek.pets.infraestructure.bd;
+
+import com.kreitek.pets.Logger;
+import com.kreitek.pets.domain.Cat;
+import com.kreitek.pets.domain.Dog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbService {
+    private static volatile DbService instance = null;
+    private List<Cat> cats = new ArrayList<>();
+    private List<Dog> dogs = new ArrayList<>();
+
+    private DbService() {
+        if (instance != null) {
+            throw new RuntimeException("Usage getInstance() method to create");
+        }
+    }
+
+    public static DbService getInstance() {
+        if (instance == null) {
+            synchronized(DbService.class) {
+                if (instance == null) {
+                    instance = new DbService();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void addNewDog(Dog dog) {
+        dogs.add(dog);
+        Logger.debug("BdService.Dog added");
+    }
+    public void addNewCat(Cat cat) {
+        cats.add(cat);
+        Logger.debug("BdService.Cat added");
+    }
+
+    public List<Cat> getCats() {
+        Logger.debug("BdService.Get " + cats.size() + " cats");
+        return new ArrayList<>(cats);
+    }
+
+    public List<Dog> getDogs() {
+        Logger.debug("BdService.Get " + cats.size() + " dogs");
+        return new ArrayList<>(dogs);
+    }
+}
