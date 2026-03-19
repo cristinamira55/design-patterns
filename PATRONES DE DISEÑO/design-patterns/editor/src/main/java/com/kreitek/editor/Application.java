b2ae66a4775ce057bad049525c0da35a83f19849
Cristina Mira Montesino <cristina.mira@nunsys.com>
2026-03-19 21:04:51 +0100

    añado JSON

--- PATRONES DE DISEÑO/design-patterns/editor/src/main/java/com/kreitek/editor/Application.java
+++ PATRONES DE DISEÑO/design-patterns/editor/src/main/java/com/kreitek/editor/Application.java
@@ -3,16 +3,9 @@ package com.kreitek.editor;
 public class Application {
 
     public static void main(String[] args) {
-        DisplayStrategy strategy;
 
-        if (args.length > 0 && args [0].equals("json")){
-            strategy = new JsonDisplayStrategy();
-        }else{
-            strategy = new TextDisplayStrategy();
-        }
         EditorFactory editorFactory = new EditorFactory();
-        Editor editor = editorFactory.getEditor(strategy);
+        Editor editor = editorFactory.getEditor(args);
         editor.run();
     }
-
 }
--- PATRONES DE DISEÑO/design-patterns/editor/src/main/java/com/kreitek/editor/EditorFactory.java
+++ PATRONES DE DISEÑO/design-patterns/editor/src/main/java/com/kreitek/editor/EditorFactory.java
@@ -1,7 +1,14 @@
 package com.kreitek.editor;
 
 public class EditorFactory {
-    public Editor getEditor(DisplayStrategy displayStrategy) {
-        return new ConsoleEditor(displayStrategy);
+    public Editor getEditor(String[] args) {
+        DisplayStrategy strategy;
+
+        if (args.length > 0 && args [0].equals("json")){
+            strategy = new JsonDisplayStrategy();
+        }else{
+            strategy = new TextDisplayStrategy();
+        }
+        return new ConsoleEditor(strategy);
     }
 }
